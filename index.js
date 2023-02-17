f1ec8d77f9cf90cce19dc8882af52ec4804c89c8
fabienfoerster <f.foerster@bevolta.com>
2023-02-17 21:29:17 +0100

    Add default for messageAttributes

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const core = require('@actions/core');
-const aws = require('aws-sdk');
+const core = require("@actions/core");
+const aws = require("aws-sdk");
 
 async function run() {
-    try {
-        const sqsUrl = core.getInput('sqs-url', { required: false });
-        const message = core.getInput('message', { required: true });
-        const messageAttributes = core.getInput('message-attributes', {required: false})
+  try {
+    const sqsUrl = core.getInput("sqs-url", { required: false });
+    const message = core.getInput("message", { required: true });
+    const messageAttributes = core.getInput("message-attributes", {
+      required: false,
+    });
 
-        const params = {
-            QueueUrl: sqsUrl,
-            MessageBody: message,
-            MessageAttributes: JSON.parse(messageAttributes)
-        };
+    const params = {
+      QueueUrl: sqsUrl,
+      MessageBody: message,
+      MessageAttributes: messageAttributes ? JSON.parse(messageAttributes) : {},
+    };
 
-        const sqs = new aws.SQS();
-        sqs.sendMessage(params, (err, resp) => {
-            if (err) {
-                throw err;
-            } else {
-                console.log(`resp ${JSON.stringify(resp, null, 2)}`);
-            }
-        })
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+    const sqs = new aws.SQS();
+    sqs.sendMessage(params, (err, resp) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(`resp ${JSON.stringify(resp, null, 2)}`);
+      }
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 module.exports = run;
 
 /* istanbul ignore next */
 if (require.main === module) {
-    run();
+  run();
 }
